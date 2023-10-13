d6a8ab39d85d8dce36333bf4852dddbcf3856d4b
exfadra <trefilov.2002@inbox.ru>
2023-10-13 15:27:24 +0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+
 import { ToDoListCompComponent } from './to-do-list-comp/to-do-list-comp.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent,
     ToDoListCompComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     CommonModule,
   ],
